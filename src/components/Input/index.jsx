9b37ed48f22b9a3e48482b90203f3c586527af50
Abel Souza <abelsouzacosta@gmail.com>
2021-05-15 15:20:32 -0300

    Adding prop-types to Input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,7 +1,15 @@
+import React from 'react';
 import './styles.css';
+
+import P from 'prop-types';
 
 export const Input = ({ searchValue, handleChange }) => {
   return (
     <input type="search" value={searchValue} onChange={handleChange} className="text-input" placeholder="Search..." />
   );
+};
+
+Input.propTypes = {
+  searchValue: P.string.isRequired,
+  handleChange: P.func.isRequired,
 };
